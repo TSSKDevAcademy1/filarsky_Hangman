32b8170022ec98009099098a5ff097cd26029a97
Vladimír Fiľarský <vladimir.filarsky@gmail.com>
2015-07-27 08:26:17 +0200

    commit

--- js/hangman.js
+++ js/hangman.js
@@ -1,37 +1,3 @@
-/* var secret = 'Nazdar';
-var lettersGuessed = 'a';
-
-function getGuessedWord(secret, lettersGuessed) {
-var length = secret.length;
-var length2 = lettersGuessed.length;
-var i;
-var j;
-var el = document.getElementById('secret');
-var array = secret.split('');
-for(i = 0;i<length;i++){
-	for(j=0;j<length2;j++){
-		if(array[i] != lettersGuessed[j]) {
-			array[i] = '_';
-          //console.log(secret[i]);
-		}
-	}
-  
-}
-	el.textContent = array;
-  return array;
-}
-
-var element;
-var g;
-var t;
-for(g=65;g<=90;g++){
-	element = document.createElement('button');
-	element.setAtribute('type','button');
-	element.textContent(String.fromCharCode(65));
-	t = decument.getElementById('buttons');
-	t.appendChild(element);
-} */
-////
 var secret ='JAVASCRIPT';
 var lettersGuessed ='';
 var counter = 1;
@@ -41,40 +7,43 @@ function getGuessedWord(secret, lettersGuessed) {
 	for(var c of secret){
 		if(lettersGuessed.indexOf(c) >=0) {
 			result +=c;
-		} else { 
+		} else {
 			result += '-';
 		}
 	} return result;
 }
 
 function onClick(event) {
-	var el = event.target;
 
+	var el = event.target;
 //deactivate button
 	el.setAttribute('disabled','disabled');
 
 //update letters guessed
-lettersGuessed += el.textContent;
+	lettersGuessed += el.textContent;
 
 
 //update secret
-var els= document.getElementById('secret');
-els.textContent = getGuessedWord(secret,lettersGuessed);
+	var els= document.getElementById('secret');
+	els.textContent = getGuessedWord(secret,lettersGuessed);
 
 //update image
-if(secret.indexOf(el.textContent) == -1) {
-	counter++;
-	var img = document.getElementById('situation');
-	img.setAttribute('src', 'images/phase' + counter );
-}
-  //check game state
-  if(counter == 5) {
-  	alert('game over');
+	if(secret.indexOf(el.textContent) == -1) {
+		counter++;
+		var img = document.getElementById('situation');
+		img.setAttribute('src', 'images/phase' + counter + '.png' );
+	}
+	//check game state
+	if(counter == 5) {
+		alert('game over');
+		var fn = document.getElementById('button');
+		for(var btn of fn){
+			btn.setAttribute('disabled', 'disabled');
+		}
+	}
 
-  	for(var btn of document.getElementById('button')){
-  		btn.setAttribute('disabled', 'disabled');
-  	}
-  }
+	if (getGuessedWord(secret, lettersGuessed).length > 0 && getGuessedWord(secret, lettersGuessed).indexOf('-') == -1) {
+		alert('Vyhral si');}
 
 }
 
@@ -87,6 +56,7 @@ for(var c of 'ABCDEFGHIJKLMNOPRSTUVWXYZ') {
 	//create button
 	var btn =  document.createElement('button');
 	btn.setAttribute('type','button');
+	btn.setAttribute('id','button');
 	btn.setAttribute('class', 'btn btn-primary');
 	btn.addEventListener('click', onClick);
 	btn.textContent = c;
@@ -95,5 +65,35 @@ for(var c of 'ABCDEFGHIJKLMNOPRSTUVWXYZ') {
 	group.appendChild(btn);
 }
 
+var btn2 =document.createElement('button');
+btn2.setAttribute('type','button');
+btn.setAttribute('id','button2');
+btn2.setAttribute('class', 'btn btn-primary');
+btn2.textContent = 'Restart';
+document.getElementById('secret2').appendChild(btn2);
+
+btn2.addEventListener('click', function onClick2() {counter=1;lettersGuessed='';
+	document.getElementById('buttons').innerHTML = "";
+	document.getElementById('secret').innerHTML = "";
+	el.textContent = getGuessedWord(secret,lettersGuessed)
+	for(var c of 'ABCDEFGHIJKLMNOPRSTUVWXYZ') {
+		//create button
+		var btn =  document.createElement('button');
+		btn.setAttribute('type','button');
+		btn.setAttribute('id','button');
+		btn.setAttribute('class', 'btn btn-primary');
+		btn.addEventListener('click', onClick);
+		btn.textContent = c;
+
+		//append button
+		group.appendChild(btn);
+		document.getElementById('situation').setAttribute('src', 'images/phase1.png' );
+	}})
+btn2.textContent = 'Restart';
+document.getElementById('secret2').appendChild(btn2);
 console.log('initialized');
+
+
+
+
 
